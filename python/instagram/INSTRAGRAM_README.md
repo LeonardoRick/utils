cf4279f0f17a47da8a95a48853c7077cc039862d
Leonardo Rick <leonardo.rick@unbabel.com>
2023-10-02 21:57:06 +0100

    update to consider scenario where we should consider more than one json file (like followers.json, followers_1.json and so on)

--- python/instagram/INSTRAGRAM_README.md
+++ python/instagram/INSTRAGRAM_README.md
@@ -5,8 +5,8 @@ scripts to work with instagram
 ### `1) followers_diff.py`
 it's is used to check whether your followers are following you back.
 
-1. Go to your instagram account under: Settings -> Your activitie -> Download your information
+1. Go to your instagram account under: Settings -> Your activity -> Download your information
 2. Request the download of only your followers and following information as .json and
-3. Past both scripts (following.json and followers.json) on the same folder
+3. Past files (following.json, following_1.json, followers.json, followers_1.json and so on..) on the script folder (followers_diff.py)
 4. Running the script will output a list of users that are not following you back.
 
--- python/instagram/followers_diff.py
+++ python/instagram/followers_diff.py
@@ -1,22 +1,45 @@
 import json
 
-following = None
-followers = None
 
-with open('following.json') as f:
-    following = json.load(f)
-with open('followers.json') as f:
-    followers = json.load(f)
+def loadJson(filePrefix):
+    accounts = []
+    count = 0
+    while True:
+        try:
+            # if the number is 0, consider following.json, if its more than 0, consider
+            # following_1.json, following_2.json and so on
+            fileName = f'{filePrefix}{"_" + str(count) if count > 0 else ""}.json'
+            with open(fileName) as f:
+                accounts.append(json.load(f))
+                count += 1
+        except FileNotFoundError:
+            break
+        except Exception as e:
+            print(e)
+            break
+    return accounts
 
 
-followingList = list(
-    map(
-        lambda f: f['string_list_data'][0]['value'],
-        following['relationships_following'],
-    )
-)
+def createListFromJson(data, key=None):
+    value = data[key] if key else data
+    return list(map(lambda f: f['string_list_data'][0]['value'], value))
 
-followersList = list(map(lambda f: f['string_list_data'][0]['value'], followers))
+
+followingFileName = 'following'
+followersFileName = 'followers'
+followingList = []
+followersList = []
+# list of jsons considering multiple files (index 0 will
+#  be relative to following.json, index 1 will be relative
+# to following_1.json and so on)
+following = loadJson('following')
+followers = loadJson('followers')
+
+for f in following:
+    followingList.extend(createListFromJson(f, 'relationships_following'))
+
+for r in followers:
+    followersList.extend(createListFromJson(r))
 
 for f in followingList:
     if f not in followersList:
